2b6cd76d2b74c9397e74d7ad1bb51db4366a6800
Aditya <adi1998.2010@gmail.com>
2017-07-25 23:55:07 +0530

    a

--- letsplay.py
+++
@@ -1,6 +0,0 @@
-import scipy.misc as smp
-import numpy as np
-a=map(int,(file("../l3ts_pl4y_1.txt").read()).split())
-a=np.array(a).reshape((887,900,3))
-img=smp.toimage(a)
-smp.imsave("temp.png",a)
